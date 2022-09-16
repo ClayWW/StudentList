7decdc7e3b01a6c716c173e2741455a86578b690
ClayWW <claywolfang@gmail.com>
2022-09-16 13:53:48 -0400

    Added comments to all the functions

--- slist.c
+++ slist.c
@@ -7,6 +7,16 @@ typeStudent *root;
 typeStudent *tail;
 
 typeStudent* createStudent(){
+    /*
+    PARAMETERS: none
+    RETURNS: a student (typeStudent)
+
+    Our buffer is defined above as 128. For the first name we first create the variable, prompt the user for their input,
+    we then alter the string to remove the \n character at the end (which has been the bane of my existence this project), and then
+    once fully prepped, we attribute the first name to the first field of the student. We repeat this process for last name, id, year, and graduation year.
+    Id is a long int and grad year is an int so for those two specific cases we go through the steps of converting them from a char to their respective 
+    data types. Our final result is a completed student (aside from their next and prev fields which remain NULL for the time being).
+    */
     typeStudent* student = (typeStudent*)malloc(sizeof(typeStudent));
     int control;
 
@@ -50,7 +60,17 @@ typeStudent* createStudent(){
     return student;
 }
 
-void addStudent(typeStudent **root, typeStudent **tail, typeStudent* newStudent){ 
+void addStudent(typeStudent **root, typeStudent **tail, typeStudent* newStudent){
+    /*
+    PARAMETERS: typeStudent **root: a double pointer that points to the pointer of the root
+                typeStudent **tail: a double pointer that points to the pointer of the tail
+                typeStudent* newStudent: The student that is being added to the doubly linked list
+    RETURNS: none
+
+    We first check the case of whether or not the root is null, meaning that our doubly linked list does not exist yet. Should this be the case,
+    we establish the newStudent as both the root and tail since it is the only student. Otherwise, we begin at the tail of the list and push the new student
+    onto the end of the list and reestablish the tail.
+    */
     if(*root == NULL){
 		*root = newStudent;
 		*tail = newStudent;
@@ -73,7 +93,23 @@ void addStudent(typeStudent **root, typeStudent **tail, typeStudent* newStudent)
 	}
 }
 
-void deleteStudent(typeStudent** root, typeStudent** tail, char* delete){ //results in a segmentation fault, this is the issue
+void deleteStudent(typeStudent** root, typeStudent** tail, char* delete){
+    /*
+        PARAMETERS: typeStudent** root: a double pointer that points to the pointer of the root
+                    typeStudent **tail: a double pointer that points to the pointer of the tail
+                    char* delete: the last name that you are trying to totally remove from the doubly linked list
+        RETURNS: None
+
+        We first establish a current value as our root which will be our starting point. We enter a while loop that continues so long
+        as the current student is not a NULL value. using strcmp(string compare), we check to see if the value between the students last name field
+        matches the inputted last name that was entered by the user. If the root has the last name we are looking for and is the only node, we set the root to null.
+        Otherwise, if the root's last name matches the last name we are looking for we disconnect the root node from the next, and then reestablish the root as the student,
+        that was to the right of our deleted root. If we are looking to delete a student who is at the tail of the list, we disconnect the tail from the student before it and
+        then reestablish the tail as the student to the left of the previous tail. If neither of these cases happen, it means that the student is in the middle of the
+        list, we merely have to connect the student behind our current studnet to the student ahead of our current student and vice versa. Should none of these cases be true,
+        meaning that the student does not have the last name we are looking for, we shift to the next one and repeat the process until we reach the end of our list.
+    */
+     //results in a segmentation fault, this is the issue
     //(EDIT) no segmentation fault now which is good, but it also doesn't work which is bad
     typeStudent* current = *root;
     //printf("0");
@@ -97,7 +133,7 @@ void deleteStudent(typeStudent** root, typeStudent** tail, char* delete){ //resu
             //printf("9");
             } else if(current->next == NULL){
                 //printf("10");
-                //current->prev->next = NULL;
+                current->prev->next = NULL;
                 //printf("11");
                 *tail = current->prev;
                 //printf("12");
@@ -109,7 +145,7 @@ void deleteStudent(typeStudent** root, typeStudent** tail, char* delete){ //resu
                 //printf("15");
             }
         }
-        printf("AA");
+        //printf("AA");
         current = current->next;
     }
     /*
@@ -163,6 +199,14 @@ void deleteStudent(typeStudent** root, typeStudent** tail, char* delete){ //resu
 }
 
 void printForwards(typeStudent** root){ //works
+    /*
+        PARAMETERS: typeStudent** root: a double pointer that points to the pointer of the root
+        RETURNS: None
+
+        Starts at the beginning of the doubly linked list and loops through it so long as the student is not of NULL value. At every iteration,
+        the function prints the student's first and last name. It then shifts to the right and repeats the process until the while loop ends (meaning we have reached
+        the end of the list).
+    */
     typeStudent* current = *root;
     while(current != NULL){
         printf("%s %s\n", current->first, current->last);
@@ -171,6 +215,14 @@ void printForwards(typeStudent** root){ //works
 }
 
 void printBackwards(typeStudent** tail){ //works
+    /*
+        PARAMETERS: typeStudent** tail: a double pointer that points to the pointer of the tail
+        RETURNS: None
+
+        SStarts at the end of the doubly linked list and loops through it so long as the student is not of NULL value. At every iteration,
+        the function prints the student's first and last name. It then shifts to the left and repeats the process until the while loop ends (meaning we have reached
+        the front of the list).
+    */
     typeStudent* current = *tail;
     while(current != NULL){
         printf("%s %s\n", current->first, current->last);
@@ -179,6 +231,13 @@ void printBackwards(typeStudent** tail){ //works
 }
 
 void quit(typeStudent** root){ //quits but may need to check if it properly deconstructs everything
+    /*
+        PARAMETERS: typeStudent** root: a double pointer that points to the pointer of the root
+        RETURNS: None
+
+        We start at the beginning of our doubly linked list, we then cycle through the entire list. While doing so, we continually free the previous
+        student's memory until we are left with nothing but our current value left. We then free the final student and exit from the program.
+    */
     typeStudent *current = *root;
     while(current != NULL){
         typeStudent *old = current;
